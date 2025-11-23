3c7b173a10de003cfe99a36c8baca8c2eba72790
Satyam1209 <satyamsingh12092000@gmail.com>
2025-11-23 06:03:11 -0800

    aws s3 bucket and docker file

--- NetworkSecurity/constant/training_pipeline/__init__.py
+++ NetworkSecurity/constant/training_pipeline/__init__.py
@@ -58,7 +58,7 @@ MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
 MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
 MODEL_TRAINER_OVER_FIITING_UNDER_FITTING_THRESHOLD: float = 0.05
 
-TRAINING_BUCKET_NAME = "netwworksecurity"
+TRAINING_BUCKET_NAME = "networksecurity1209"
 
 SAVED_MODEL_DIR = os.path.join('saved_models')
 MODEL_FILE_NAME  = 'model.pkl'
